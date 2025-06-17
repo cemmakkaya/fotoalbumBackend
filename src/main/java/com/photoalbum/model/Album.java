88d380939693cdb05dee46553700401263c03829
topptobi <tobias.topp@gmx.ch>
2025-06-17 16:15:07 +0200

    Added Controller and a connection with postgres

--- src/main/java/com/photoalbum/model/Album.java
+++ src/main/java/com/photoalbum/model/Album.java
@@ -3,21 +3,25 @@ package com.photoalbum.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @Entity
-public class Photo {
+public class Album {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
-    private String description;
-    private String url;
-    private Long userId;
 
-    @ManyToOne
-    @JoinColumn(name = "album_id")
-    private Album album;
+    private int year;
+
+    @Column(length = 85)
+    private String description;
+
+    @ElementCollection
+    private List<String> imageUrls;
+
+    private Long userId;
 }
---
+++ src/main/java/com/photoalbum/service/AlbumService.java
@@ -0,0 +1,37 @@
+package com.photoalbum.service;
+
+import com.photoalbum.model.Album;
+import com.photoalbum.repository.AlbumRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlbumService {
+
+    private final AlbumRepository repository;
+
+    public AlbumService(AlbumRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Album> getAll() {
+        return repository.findAll();
+    }
+
+    public Album getOne(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<Album> getByUser(Long userId) {
+        return repository.findByUserId(userId);
+    }
+
+    public Album save(Album album) {
+        return repository.save(album);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
